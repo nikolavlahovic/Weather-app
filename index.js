17d00655bd2e364198904fb11c3d55738ec4909e
Nikola Vlahovic <vlahovi.nikola@gmail.com>
2022-04-07 20:35:40 +0200

    Added weather app

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log('1');
+let weather = {
+    apiKey: 'da3367f1077fe0b3ae65c2f9435ba806',
+    getWeather: function (city) {
+        fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q='
+            + city
+            + '&units=metric&appid='
+            + this.apiKey
+        )
+            .then(response => response.json())
+            .then(data => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerHTML = 'Weather in ' + name;
+        document.querySelector('.degrees').innerHTML = Math.floor(temp) + '&deg';
+        document.querySelector('.humidity').innerHTML = 'Humidity:' + humidity + '%';
+        document.querySelector('.description').innerHTML = description;
+        document.querySelector('.wind').innerHTML = 'Wind speed: ' + speed + 'km/h';
+        document.querySelector('.icon').src = 'http://openweathermap.org/img/w/' + icon + '.png';
+    }
+}
+
+document.querySelector('#btn').addEventListener('click', function () {
+    let city = document.querySelector('#search').value;
+    weather.getWeather(city);
+});
+
+
+
+
+
